12f26c575296a3ea2b61c21e1e749c95f0934c3f
Andrew Bell <andrew.bell@icloud.com>
2016-04-17 12:30:22 +1000

    Install packages

--- Vagrantfile
+++ Vagrantfile
@@ -8,8 +8,8 @@
 Vagrant.configure(2) do |config|
 
   config.vm.box       = "centos/7"
-  config.vm.host_name = "dev.hostname.vm"
-  config.vm.define    :app
+  config.vm.host_name = "vpnserver.vm"
+  config.vm.define    :vpnserver
 
 #  config.vm.network "private_network", ip: "192.168.10.10"
 #  config.vm.network "forwarded_port", guest: 80, host: 8084
--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,19 @@
-node 'dev.hostname.vm' {
+node 'vpnserver.vm' {
   notify { 'my_message':
     message => hiera('my_message'),
+  }
+
+  # Step 1 - Installing OpenVPN
+  package {'epel-release':
+    provider => 'yum',
+    ensure   => installed,
+  } ->
+  package {'openvpn':
+    provider => 'yum',
+    ensure   => installed,
+  } ->
+  package {'easy-rsa':
+    provider => 'yum',
+    ensure   => installed,
   }
 }
